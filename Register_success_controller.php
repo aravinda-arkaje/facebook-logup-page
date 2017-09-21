3d4efc4188a171eb0d70f25aa35c1bd90ff335d4
aravinda-arkaje <aravindaarkaje@gmail.com>
2017-09-21 20:49:27 +0530

    Update and rename Register_success.php to Register_success_controller.php

--- Register_success_controller.php
+++ Register_success_controller.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Register_success extends CI_Controller {
+class Register_success_controller extends CI_Controller {
 
 	function __construct() {
 		parent::__construct();
